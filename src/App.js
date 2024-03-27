7b6d15d2dff80cbc196fab8c1a384afde3ef42bb
Sebastian Llanes <sebastianllanessaavedra@gmail.com>
2024-03-27 11:27:19 -0300

    Enlace Navbar con CSS

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './Pages/About';
 import Knownledge from './Pages/Knownledge';
 import Experience from './Pages/Experience';
 import Contact from './Pages/Contact';
+import './App.css';
 
 const App = () => {
   return (
