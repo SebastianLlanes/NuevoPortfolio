2e350f7efbdccbe1e69fc656f97074eb673bb9a0
Sebastian Llanes <sebastianllanessaavedra@gmail.com>
2024-03-29 15:33:33 -0300

    Mejora del Navbar en ux

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import '../App.css';
+import { useLocation } from 'react-router-dom'; // Importa useLocation de react-router-dom
 
 function Navbar() {
+  const location = useLocation(); // Obtiene la ubicación actual
+
   return (
     <nav className='navbar'>
       <ul className='ul-navbar'>
-        <li className='li-navbar'><a href="/">Home</a></li>
-        <li className='li-navbar'><a href="/Experience">Experience</a></li>
-        <li className='li-navbar'><a href="/Knownledge">Knownledge</a></li>
-        <li className='li-navbar'><a href="/about">About me</a></li>                  
-        <li className='li-navbar'><a href="/Contact">Contact</a></li>
+        {/* Usa lógica condicional para renderizar los elementos del navbar */}
+        {location.pathname !== '/' && (
+          <li className='li-navbar'>
+            <a href="/">Home</a>
+          </li>
+        )}
+        {location.pathname !== '/Experience' && (
+          <li className='li-navbar'>
+            <a href="/Experience">Experience</a>
+          </li>
+        )}
+        {location.pathname !== '/Knownledge' && (
+          <li className='li-navbar'>
+            <a href="/Knownledge">Knownledge</a>
+          </li>
+        )}
+        {location.pathname !== '/about' && (
+          <li className='li-navbar'>
+            <a href="/about">About me</a>
+          </li>
+        )}
+        {location.pathname !== '/Contact' && (
+          <li className='li-navbar'>
+            <a href="/Contact">Contact</a>
+          </li>
+        )}
       </ul>
     </nav>
   );
