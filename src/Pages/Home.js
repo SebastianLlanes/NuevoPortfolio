6640c9d7507866c3c4c031fe8d04e5a7d3b2fe01
Sebastian Llanes <sebastianllanessaavedra@gmail.com>
2024-04-08 11:08:08 -0300

    Commit desde nuevo entorno

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,39 +8,34 @@ import ProfileImg from '../assets/images/Picsart_22-11-07_14-04-00-132.png';
 import '../App.css';
 
 const Home = () => {
- //Efecto texto h2 
+ //Efecto texto h2
 const [text, setText] = useState(["", ""]);
 const typingSpeed = 100;
-
- useEffect(() => {
-   const typingIntervals = [];
-   const words = ["Frontend", "Developer"];
-
-   words.forEach((word, index) => {
-     let currentIndex = 0;
-
-     const intervalId = setInterval(() => {
-       setText((prevText) => {
-         if (prevText[index].length < word.length) {
-           return [
-             ...prevText.slice(0, index),
-             word.substring(0, currentIndex + 1),
-             ...prevText.slice(index + 1),
-           ];
-         } else {
-           clearInterval(intervalId);
-           return prevText;
-         }
-       });
-       currentIndex++;
-     }, typingSpeed);
-
-     typingIntervals.push(intervalId);
-   });
-
-   return () => {
-     typingIntervals.forEach((intervalId) => clearInterval(intervalId));
-   };
+useEffect(() => {
+  const typingIntervals = [];
+  const words = ["Frontend", "Developer"];
+  words.forEach((word, index) => {
+    let currentIndex = 0;
+    const intervalId = setInterval(() => {
+      setText((prevText) => {
+        if (prevText[index].length < word.length) {
+          return [
+            ...prevText.slice(0, index),
+            word.substring(0, currentIndex + 1),
+            ...prevText.slice(index + 1),
+          ];
+        } else {
+          clearInterval(intervalId);
+          return prevText;
+        }
+      });
+      currentIndex++;
+    }, typingSpeed);
+    typingIntervals.push(intervalId);
+  });
+  return () => {
+    typingIntervals.forEach((intervalId) => clearInterval(intervalId));
+  };
  }, []);
 
   return (
