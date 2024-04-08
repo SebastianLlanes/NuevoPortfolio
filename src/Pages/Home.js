2473c40ff95cddd31c496b5f8baf57a1b2bcfab7
Sebastian Llanes <sebastianllanessaavedra@gmail.com>
2024-04-08 09:46:01 -0300

    Typing text

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from '../Components/Navbar';
 import Facebook from '../assets/icons/facebook.png';
 import Linkedin from '../assets/icons/linkedin.png';
@@ -8,6 +8,40 @@ import ProfileImg from '../assets/images/Picsart_22-11-07_14-04-00-132.png';
 import '../App.css';
 
 const Home = () => {
+ //Efecto texto h2 
+const [text, setText] = useState(["", ""]);
+const typingSpeed = 100;
+
+ useEffect(() => {
+   const typingIntervals = [];
+   const words = ["Frontend", "Developer"];
+
+   words.forEach((word, index) => {
+     let currentIndex = 0;
+
+     const intervalId = setInterval(() => {
+       setText((prevText) => {
+         if (prevText[index].length < word.length) {
+           return [
+             ...prevText.slice(0, index),
+             word.substring(0, currentIndex + 1),
+             ...prevText.slice(index + 1),
+           ];
+         } else {
+           clearInterval(intervalId);
+           return prevText;
+         }
+       });
+       currentIndex++;
+     }, typingSpeed);
+
+     typingIntervals.push(intervalId);
+   });
+
+   return () => {
+     typingIntervals.forEach((intervalId) => clearInterval(intervalId));
+   };
+ }, []);
 
   return (
       <div className='home-div'>
@@ -22,7 +56,10 @@ const Home = () => {
           </div>
           <div className='titles-home-div'>
             <h1>Sebastián Llanes</h1>
-            <h2>Frontend Developer</h2>
+            <h2>
+            {text[0]} {text[1]}
+              <span className="cursor">|</span> {/* Cursor intermitente */}
+            </h2>
           </div>
         </section>
         <footer className='footer'>
