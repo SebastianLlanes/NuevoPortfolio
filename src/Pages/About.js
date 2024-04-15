b72b02afb10c220d9576ee00c0db79b8b97204d2
Sebastian Llanes <sebastianllanessaavedra@gmail.com>
2024-04-15 15:03:43 -0300

    About me ImageSlide, mejora

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -7,10 +7,6 @@ const About = () => {
         <h2>About me</h2>
         <section className='about-section'>          
           <ImageSlider />
-          {/* <div className='titles-home-div'>
-            <h1>Welcome to my website!</h1>
-            <p>This is the homepage of my React.js website.</p>
-          </div> */}
         </section>
       </div>
   );
