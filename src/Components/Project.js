654b6fc9968baddb57baee9abf82bd83290b2013
Sebastian Llanes <sebastianllanessaavedra@gmail.com>
2024-04-16 16:37:33 -0300

    Finalizado Projects! (Definir hoover de las cards)

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -2,35 +2,49 @@ import React from "react";
 import CssIcon from '../assets/icons/css.svg';
 import JavascriptIcon from '../assets/icons/js.svg';
 import ReactIcon from '../assets/icons/react.svg';
+import SantaGulaIcon from '../assets/images/santagula.jpg';
+import AntroIcon from '../assets/images/antro.png';
+import ToDoAppIcon from '../assets/images/todoapp.png';
 import '../App.css';
 
 function Project() {
 
   const projectContent = [
-    { project: 'Menú digital con pre orden', description: 'Comenzó como un proyecto informativo al que luego se le agrego una dinámica para hacer una preorden por WhatsApp', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon },
-    { project: 'ToDo App', description: 'Task Machine para quienes desean tener orden con un simple ayuda memoria.', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon },
-    { project: 'Weather App', description: 'App desarrollada para obtener el comportamiento del clima.',techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon },
+    { project: 'Menú digital con pre orden', image: SantaGulaIcon, description: 'Comenzó como un proyecto informativo al que luego se le agrego una dinámica para hacer una preorden por WhatsApp', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://santa-gula-menu.vercel.app/' },
+    { project: 'Menú digital', image: AntroIcon, description: 'Menú digital con portada, botones de bebidas y comidas, enlaces a redes sociales y Spotify', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://antro-bar.vercel.app' },
+    { project: 'ToDo App', image: ToDoAppIcon, description: 'Task Machine para quienes desean tener orden con un simple ayuda memoria.', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://sebastianllanes.github.io/ToDoApp/' },
+    // { project: 'Weather App', image: , description: 'App desarrollada para obtener el comportamiento del clima.',techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon },
     
   ]
+
+  const handleClick = (url) => {
+    setTimeout(() => {
+      window.open(url, '_blank');
+    }, 500); 
+  };
+  
 
   return (
     <>
     {projectContent.map((project, index) => (
-        <div className="project-card-sm" key={index}>
-            <div className="card-border-gradient"></div>
-                <div className="card-container">
-                    <img src="https://unblast.com/wp-content/uploads/2018/08/Gradient-Mesh-22.jpg" alt="" />
-                    <div className="wrapper">
-                        <h3>{project.project}</h3>
-                        <p> {project.description} </p>
-                        <span> 
-                            <img src={project.techOne} alt='Javasrcipt' />  
-                            <img src={project.techTwo} alt='React' /> 
-                            <img src={project.techThree} alt='CSS' />    
-                        </span>           
-                    </div>
+         <a href={project.url} onClick={() => handleClick(project.url)} key={index}  className="project-card-sm" target="_blank" rel="noopener noreferrer">
+            <div>
+                <div className="card-border-gradient"></div>
+                    <div className="card-container">
+                        <img src= {project.image} alt= {project.project} />
+                        <div className="wrapper">
+                            <h3>{project.project}</h3>
+                            <p> {project.description} </p>
+                            <span> 
+                                <img src={project.techOne} alt='Javasrcipt' />  
+                                <img src={project.techTwo} alt='React' /> 
+                                <img src={project.techThree} alt='CSS' />    
+                            </span>           
+                        </div>
+                </div>
             </div>
-        </div>
+        </a>
+         
         ))}     
         </>
        );
