dbbc85c721509c213f8e6eae4064ae28946ef087
Sebastian Llanes <sebastianllanessaavedra@gmail.com>
2024-05-31 11:41:33 -0300

    New Project Add

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -5,6 +5,7 @@ import ReactIcon from '../assets/icons/react.svg';
 import SantaGulaIcon from '../assets/images/santagula.jpg';
 import AntroIcon from '../assets/images/antro.png';
 import ToDoAppIcon from '../assets/images/todoapp.png';
+import SubscriptionTrackerIcon from '../assets/images/popcorn2.png';
 import '../App.css';
 
 function Project() {
@@ -13,6 +14,7 @@ function Project() {
     { project: 'Menú digital con pre orden', image: SantaGulaIcon, description: 'Comenzó como un proyecto informativo al que luego se le agrego una dinámica para hacer una preorden por WhatsApp', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://santa-gula-menu.vercel.app/' },
     { project: 'Menú digital', image: AntroIcon, description: 'Menú digital con portada, botones de bebidas y comidas, enlaces a redes sociales y Spotify', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://antro-bar.vercel.app' },
     { project: 'ToDo App', image: ToDoAppIcon, description: 'Task Machine para quienes desean tener orden con un simple ayuda memoria.', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://sebastianllanes.github.io/ToDoApp/' },
+    { project: 'Subscriptions Tracker', image: SubscriptionTrackerIcon, description: 'App para control de gastos en subscripciones.', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://subscriptions-tracker-one.vercel.app/' },
     // { project: 'Weather App', image: , description: 'App desarrollada para obtener el comportamiento del clima.',techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon },
     
   ]
