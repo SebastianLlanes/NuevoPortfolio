d0c7ab3e40bf5a0810e3934f29a541875d757b53
Sebastian Llanes <sebastianllanessaavedra@gmail.com>
2026-02-03 10:03:11 -0300

    feat: new project add

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -3,6 +3,7 @@ import CssIcon from '../assets/icons/css.svg';
 import JavascriptIcon from '../assets/icons/js.svg';
 import ReactIcon from '../assets/icons/react.svg';
 import SantaGulaIcon from '../assets/images/santagula.jpg';
+import SantaGulaGestion from '../assets/images/gestiondepedidos.png';
 import AntroIcon from '../assets/images/antro.png';
 import ToDoAppIcon from '../assets/images/todoapp.png';
 import SubscriptionTrackerIcon from '../assets/images/popcorn2.png';
@@ -14,6 +15,7 @@ function Project() {
 
   const projectContent = [
     { project: 'Menú digital con pre orden', image: SantaGulaIcon, description: 'Comenzó como un proyecto informativo al que luego se le agrego una dinámica para hacer una preorden por WhatsApp', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://santa-gula-menu.vercel.app/' },
+    { project: 'Aplicación para gestión de pedidos', image: SantaGulaGestion, description: 'Gestión de pedidos en tiempo real. Almacenamiento de datos en Firebase para confección de informes para análisis en la toma de decisiones ', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://santagula-gestion.vercel.app/' },
     { project: 'Menú digital', image: AntroIcon, description: 'Menú digital con portada, botones de bebidas y comidas, enlaces a redes sociales y Spotify', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://antro-bar.vercel.app' },
     { project: 'ToDo App', image: ToDoAppIcon, description: 'Task Machine para quienes desean tener orden con un simple ayuda memoria.', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://sebastianllanes.github.io/ToDoApp/' },
     { project: 'Subscriptions Tracker', image: SubscriptionTrackerIcon, description: 'App para control de gastos en subscripciones.', techOne: JavascriptIcon, techTwo: ReactIcon, techThree: CssIcon, url: 'https://subscriptions-tracker-one.vercel.app/' },
